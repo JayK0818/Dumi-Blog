b312310e00e7027186ee8041ccbf5ebcf2bfb084
JayK0818 <1129594783@qq.com>
2024-11-16 21:38:06 +0800

    docs: config

--- .dumirc.ts
+++ .dumirc.ts
@@ -9,14 +9,59 @@ export default defineConfig({
         link: '/javascript'
       },
       {
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
         title: 'NodeJS',
+=======
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+        title: 'Vue',
+        link: '/vue'
+      },
+      {
+        title: 'React',
+        link: '/react'
+      },
+      {
+        title: 'Node',
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
         link: '/node'
       }
     ],
     lastUpdated: true,
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
     nprogress: true,
     showLineNum: true
   },
   base: '/',
+=======
+    showLineNum: true, // 是否在代码块中展示行号
+    nprogress: true, //
+    base: './'
+  },
+>>>>>>> Stashed changes
+=======
+    showLineNum: true, // 是否在代码块中展示行号
+    nprogress: true, //
+    base: './'
+  },
+>>>>>>> Stashed changes
+=======
+    showLineNum: true, // 是否在代码块中展示行号
+    nprogress: true, //
+    base: './'
+  },
+>>>>>>> Stashed changes
   presets: ['@dumijs/preset-vue']
 });
